667c1246e6a1f45bfd24a1bbd4714396391cb828
Hong Duc Cai <hong.duc.cai@gmail.com>
2021-04-27 19:59:21 +0200

    click on year to search and started on dropdown

---
+++ memoroad/src/common-components/AppDropdown.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: "Dropdown",
+    setup() {
+        
+    },
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
