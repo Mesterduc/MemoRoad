702fad24dc2b6aff463447c65d890454c4371aa3
Hong Duc Cai <hong.duc.cai@gmail.com>
2021-04-28 19:10:23 +0200

    dropdown

--- memoroad/src/common-components/Dropdown/AppDropdown.vue
+++ memoroad/src/common-components/Dropdown/AppDropdown.vue
@@ -1,28 +1,19 @@
 <template>
-	<!-- <template slot="toggler"> -->
-        <div class="hej">
-
-	<div @click="toggle">
-			<button class="knap">show all
-                <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              class="arrow ml-1 h-5 w-5 fill-current text-gray-700"
-            >
-              <path
-                d="M15.3 9.3a1 1 0 0 1 1.4 1.4l-4 4a1 1 0 0 1-1.4 0l-4-4a1 1 0 0 1 1.4-1.4l3.3 3.29 3.3-3.3z"
-              ></path>
-            </svg>
-            </button>
-			<!-- <input type="text" class="knap" /> -->
-	</div>
-	<!-- </template> -->
-	<dropdown-content :sharedState="sharedState">
-		<dropdown-item>Action 1</dropdown-item>
-		<dropdown-item>Action 2</dropdown-item>
-		<dropdown-item>Action 3</dropdown-item>
-	</dropdown-content>
-        </div>
+	<section class="dropdown">
+		<button class="knap" @click="toggle">
+			{{ buttonValue }}
+			<svg viewBox="0 0 24 24" class="arrow ml-1 h-5 w-5 fill-current text-gray-700">
+				<path
+					d="M15.3 9.3a1 1 0 0 1 1.4 1.4l-4 4a1 1 0 0 1-1.4 0l-4-4a1 1 0 0 1 1.4-1.4l3.3 3.29 3.3-3.3z"
+				></path>
+			</svg>
+		</button>
+		<article class="dropdown-item" v-if="isOpen" @click="toggle">
+			<span class="dropdown-item__value" @click="getValue">show all</span>
+			<span class="dropdown-item__value" @click="getValue">latest</span>
+			<span class="dropdown-item__value" @click="getValue">oldest</span>
+		</article>
+	</section>
 </template>
 
 <script lang="ts">
@@ -33,44 +24,58 @@ import dropdownItem from './AppDropdownItem.vue'
 export default defineComponent({
 	name: 'Dropdown',
 	setup(context) {
-		const sharedState = ref(false)
+		const isOpen = ref(false)
+		const buttonValue = ref('show all')
 
 		function toggle() {
-			sharedState.value = !sharedState.value
+			isOpen.value = !isOpen.value
+		}
+		function getValue(e: any) {
+			buttonValue.value = e.target.innerHTML
 		}
 
-		return { sharedState, toggle }
+		return { isOpen, toggle, buttonValue, getValue }
 	},
-	props: {
-		sharedState: {
-			type: Boolean,
-		},
-        closeDropdown: {
-            type: Boolean
-        },
-	},
-	components: {
-		dropdownContent,
-		dropdownItem,
-	},
+	props: {},
+	components: {},
 })
 </script>
 
 <style lang="scss" scoped>
-.hej{
-    position: relative;
-    width: 100px;
+.knap {
+	width: 100%;
+	padding: 5px;
+	border: 1px solid #c1c1c1;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	height: 30px;
+	border-radius: 2px;
 }
-.knap{
-    width: 100%;
-    padding: 5px;
-        border: 1px solid #c1c1c1;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    height: 30px;
+.arrow {
+	height: 16px;
 }
-.arrow{
-    height: 16px;
+.dropdown {
+	position: relative;
+	background: white;
+	min-width: 125px;
+
+	&-item {
+		position: absolute;
+		top: 29px;
+		left: 0;
+		display: flex;
+		flex-direction: column;
+		border: 1px solid rgb(185, 185, 185);
+		width: 100%;
+		&__value {
+			padding: 2px 5px;
+
+			&:hover {
+				background: black;
+				color: white;
+			}
+		}
+	}
 }
 </style>
